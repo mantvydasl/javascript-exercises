87360964505420e6ab3e601508ba255109644380
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 17:38:37 +0200

    Create reversedString adding chars in a loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
 const reverseString = function(string) {
   let chars = string.split('');
   let reversedString = '';
+  for (let i = chars.length - 1; i >= 0; i--) {
+    reversedString += chars[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
