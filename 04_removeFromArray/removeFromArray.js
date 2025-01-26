91736a28b299b133e25ed26fa48954a13969aa06
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 19:48:30 +0200

    Fix removal of one item

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 const removeFromArray = function(array, valueToRemove) {
     let indexToRemove = array.indexOf(valueToRemove);
-    return array.splice(indexToRemove, indexToRemove);
+    array.splice(indexToRemove, 1);
+    return array;
 };
 
 // Do not edit below this line
