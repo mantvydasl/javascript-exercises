8e3c91410655449efe41afcc71d00df28e242751
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 21:10:26 +0200

    Add variables based on their divisibility

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
 const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+
     if (year % 4 === 0 && year % 100 !== 0) {
         return true;
     } else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
