4d4a9fe3885e823a8456e48812b05b439926bd40
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 20:45:23 +0200

    Return 'ERROR' if any argument is negative

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function(num1, num2) {
-    if (typeof num1 != "number" || typeof num2 != "number") {
+    if (typeof num1 != "number" || typeof num2 != "number" ||
+        num1 < 0 || num2 < 0
+    ) {
         return "ERROR";
     }
     const start = num1 < num2 ? num1 : num2;
