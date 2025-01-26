5f345ebca7b354a2def4bb24ba0c1777646463a3
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 20:34:08 +0200

    Sum numbers between start and end

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function(num1, num2) {
     const start = num1 < num2 ? num1 : num2;
     const end   = num1 > num2 ? num1 : num2;
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
