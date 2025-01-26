eb46656498cb3057d73d7ab9b6804254d9cd3664
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 17:20:50 +0200

    Change repeatString to 'ERROR' for negative num

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string, num) {
   let repeatedString = "";
+  if (num < 0) {
+    repeatString = "ERROR";
+  }
   if (num >= 0) {
     repeatedString = "";
     while (num > 0) {
