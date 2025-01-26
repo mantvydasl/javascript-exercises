40a0c71c226f5edacd21626ef30e6af29842c26b
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 19:44:20 +0200

    Find index value of item to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valueToRemove) {
+    let indexToRemove = array.indexOf(valueToRemove);
 };
 
 // Do not edit below this line
