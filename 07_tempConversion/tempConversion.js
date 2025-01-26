ec9b95897e8a0811a5b1fb7dd4a87901cb21d823
mantvydasl <mantvydas.lopeta@gmail.com>
2025-01-26 21:29:32 +0200

    Round numbers to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(tempFahrenheit) {
   let tempCelsius = (tempFahrenheit - 32) * (5 / 9);
-  return tempCelsius;
+  return parseFloat(tempCelsius.toFixed(1));
 };
 
 const convertToFahrenheit = function(tempCelsius) {
   let tempFahrenheit = (9 / 5) * tempCelsius + 32;
-  return tempFahrenheit;
+  return parseFloat(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
