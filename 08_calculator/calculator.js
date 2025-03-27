941ff47ec0c5dfe8bb0f6ac5ecd41d02b6bf4873
mantvydasl <mantvydas.lopeta@gmail.com>
2025-03-27 10:19:37 +0200

    Create sum function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
 const multiply = function() {
