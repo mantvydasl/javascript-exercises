a91c30156e493ab54b265e70c1b2b0bd982494a7
mantvydasl <mantvydas.lopeta@gmail.com>
2025-03-29 08:35:55 +0200

    Create forward array with letters only

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
 const palindromes = function (str) {
   let arr = str.toLowerCase().split('');
+  let arrForward = [];
+  for (let c of arr) {
+    if (c.toLowerCase() != c.toUpperCase()) {
+        arrForward.push(c);
+    }
+  }
   let arrReverse = arr.slice().reverse();
   return arr.every((value, index) => value === arrReverse[index]);
 };
