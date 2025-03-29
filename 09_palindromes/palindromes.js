e8106d8660475e0028d70273752192dad7f599a8
mantvydasl <mantvydas.lopeta@gmail.com>
2025-03-29 08:26:38 +0200

    Reverse array and compare to itself

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let arr = str.split('');
+  let arrReverse = arr.slice().reverse();
+  return arr.every((value, index) => value === arrReverse[index]);
 };
 
 // Do not edit below this line
